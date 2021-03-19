8a2274ef05229a03120bf31d152bada9e0c077e2
Ayush Raj Singh <ayushrajsingh@users.noreply.github.com>
2021-03-19 16:24:00 +0530

    boom boom

--- login.js
+++ login.js
@@ -1,8 +1,28 @@
-<script type="text/javascript">
-		$(".info-item .btn").click(function(){
-		  $(".container").toggleClass("log-in");
-		});
-		$(".container-form .btn").click(function(){
-		  $(".container").addClass("active");
-		});
-	</script>
+(function($) {
+  "use strict";
+$(document).ready(function() {
+  var panelOne = $('.form-panel.two').height(),
+    panelTwo = $('.form-panel.two')[0].scrollHeight;
+
+  $('.form-panel.two').not('.form-panel.two.active').on('click', function(e) {
+    e.preventDefault();
+
+    $('.form-toggle').addClass('visible');
+    $('.form-panel.one').addClass('hidden');
+    $('.form-panel.two').addClass('active');
+    $('.form').animate({
+      'height': panelTwo
+    }, 200);
+  });
+
+  $('.form-toggle').on('click', function(e) {
+    e.preventDefault();
+    $(this).removeClass('visible');
+    $('.form-panel.one').removeClass('hidden');
+    $('.form-panel.two').removeClass('active');
+    $('.form').animate({
+      'height': panelOne
+    }, 200);
+  });
+});
+})(jQuery);
