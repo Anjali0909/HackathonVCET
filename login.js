dbfea23560e05c8c5819d638a355600065785da1
Ayush Raj Singh <ayushrajsingh@users.noreply.github.com>
2021-03-19 15:45:27 +0530

    boom boom

---
+++ login.js
@@ -0,0 +1,8 @@
+<script type="text/javascript">
+		$(".info-item .btn").click(function(){
+		  $(".container").toggleClass("log-in");
+		});
+		$(".container-form .btn").click(function(){
+		  $(".container").addClass("active");
+		});
+	</script>
